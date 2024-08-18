8d3b160ce5ae67d9a6ce45ed9f058704d1a84d52
olena-yakovleva <kvitka.olena1@gmail.com>
2024-08-18 04:56:31 +0200

    add1

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   const faqItems = document.querySelectorAll('.faq-item');
+document.addEventListener('DOMContentLoaded', function () {
+  const faqItems = document.querySelectorAll('.faq-item');
 
-//   faqItems.forEach(item => {
-//     item
-//       .querySelector('.faq-content-question')
-//       .addEventListener('click', function () {
-//         const isActive = item.classList.contains('active');
+  faqItems.forEach(item => {
+    item
+      .querySelector('.faq-content-question')
+      .addEventListener('click', function () {
+        const isActive = item.classList.contains('active');
 
-//         faqItems.forEach(i => i.classList.remove('active'));
+        faqItems.forEach(i => i.classList.remove('active'));
 
-//         if (!isActive) {
-//           item.classList.add('active');
-//         }
-//       });
-//   });
-// });
+        if (!isActive) {
+          item.classList.add('active');
+        }
+      });
+  });
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   const accordion = new Accordion('.accordion-container', {
