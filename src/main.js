a004ace747e280e4ffe89d73940b208353fe28ca
olena-yakovleva <kvitka.olena1@gmail.com>
2024-08-18 04:40:32 +0200

    add file

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const faqItems = document.querySelectorAll('.faq-item');
+
+//   faqItems.forEach(item => {
+//     item
+//       .querySelector('.faq-content-question')
+//       .addEventListener('click', function () {
+//         const isActive = item.classList.contains('active');
+
+//         faqItems.forEach(i => i.classList.remove('active'));
+
+//         if (!isActive) {
+//           item.classList.add('active');
+//         }
+//       });
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordion = new Accordion('.accordion-container', {
+    duration: 300,
+    showMultiple: false,
+    onOpen: function (currentElement) {
+      console.log('Opened:', currentElement);
+    },
+    onClose: function (currentElement) {
+      console.log('Closed:', currentElement);
+    },
+  });
+});
